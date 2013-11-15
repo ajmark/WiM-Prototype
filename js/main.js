7866edf0898ed633136db733936bf9d93b1f98c3
Alex Mark <ajmark@andrew.cmu.edu>
2013-11-14 22:10:26 -0500

    general nav layout

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("header").append("<nav><ul></ul></nav>");
+    $("ul").append("<li id='home'><a>Home</a></li>");
+    $("ul").append("<li id='about'><a>About WiM</a></li>");
+    $("ul").append("<li id='events'><a>Events</a></li>");
+    $("ul").append("<li id='pdev'><a>Professional Development</a></li>");
+    $("ul").append("<li id='resc'><a>Community Resources</a></li>");
+    $("ul").append("<li id='members'><a>Members</a></li>");
+    $("ul").append("<li id='contact'><a>Contact Us</a></li>");
+});
