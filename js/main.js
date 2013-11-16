097d3ecbb00dbe42c68a62a720600afb02e015f9
Alex Mark <ajmark@andrew.cmu.edu>
2013-11-16 14:27:30 -0500

    header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-    $("header").append("<nav><ul></ul></nav>");
-    $("ul").append("<li id='home'><a>Home</a></li>");
-    $("ul").append("<li id='about'><a>About WiM</a></li>");
-    $("ul").append("<li id='events'><a>Events</a></li>");
-    $("ul").append("<li id='pdev'><a>Professional Development</a></li>");
-    $("ul").append("<li id='resc'><a>Community Resources</a></li>");
-    $("ul").append("<li id='members'><a>Members</a></li>");
-    $("ul").append("<li id='contact'><a>Contact Us</a></li>");
+	//Creates the top nav bar
+	$("header").append("<div id='interact'></div>");
+	$("#interact").append("<h5>Interact:</h5>");
+	$("#interact").append("<ul id='top-nav'></ul");
+	$("#top-nav").append("<li>Member Login</li>");
+	//Creates the logo
+	$("header").append("<div id='logo'></div>");
+	$("#logo").append("<img src='img-sprites/logo.gif'>");
+	$("#logo").append("<div id='title'></div>");
+	$("#title").append("<h4>Allegheney Health Network<br><span>Women in Medicine</span></h4>");
+	// Creates the main nav bar
+    $("header").append("<nav><ul id='main-nav'></ul></nav>");
+    $("#main-nav").append("<li id='home'><a>Home</a></li>");
+    $("#main-nav").append("<li id='about'><a>About WiM</a></li>");
+    $("#main-nav").append("<li id='events'><a>Events</a></li>");
+    $("#main-nav").append("<li id='pdev'><a>Professional Development</a></li>");
+    $("#main-nav").append("<li id='resc'><a>Community Resources</a></li>");
+    $("#main-nav").append("<li id='members'><a>Members</a></li>");
+    $("#main-nav").append("<li id='contact'><a>Contact Us</a></li>");
 });
