20ff3dbc8f2afa3cd2fd220be97472ad98fe3556
Jake Kushner <jakekushner@cmu.edu>
2013-11-20 00:41:40 -0500

    fixed a ton of css. and homepage work

--- js/main.js
+++ js/main.js
@@ -21,18 +21,33 @@ $(document).ready(function() {
 	$("#title").append("<h4>Allegheney Health Network<br><span>Women in Medicine</span></h4>");
 	// Creates the main nav bar
     $("header").append("<nav><ul id='main-nav'></ul></nav>");
-    $("#main-nav").append("<li id='home'><a href='#'>Home</a></li>");
-    $("#main-nav").append("<li id='about'><a href='#'>About WiM</a></li>");
-    $("#main-nav").append("<li id='events'><a href='#'>Events</a></li>");
-    $("#main-nav").append("<li id='pdev'><a href='#'>Professional Development</a></li>");
-    $("#main-nav").append("<li id='resc'><a href='#'>Community Resources</a></li>");
-    $("#main-nav").append("<li id='members'><a href='#'>Members</a></li>");
-    $("#main-nav").append("<li id='contact'><a href='#'>Contact Us</a></li>");
+    $("#main-nav").append("<li class='nav-home'><a href='index.html'>Home</a></li>");
+    $("#main-nav").append("<li class='nav-about'><a href='#'>About WiM</a></li>");
+    $("#main-nav").append("<li class='nav-events'><a href='#'>Events</a></li>");
+    $("#main-nav").append("<li class='nav-pdev'><a href='#'>Professional Development</a></li>");
+    $("#main-nav").append("<li class='nav-resc'><a href='#'>Community Resources</a></li>");
+    $("#main-nav").append("<li class='nav-members'><a href='#'>Members</a></li>");
+    $("#main-nav").append("<li class='nav-contact'><a href='#'>Contact Us</a></li>");
 	 
 	 
-	 var aboutdropdown = "<ul class='dropdown drop-about'><li><a href='#'>Mission and History</a></li><li><a href='#'>Program Administrators</a></li></ul>";
+	 var aboutDropdown = "<ul class='dropdown drop-about'><li><a href='mission.html'>Mission and History</a></li><li><a href='#'>Program Administrators</a></li></ul>";
+	 
+	 var eventsDropdown = "<ul class='dropdown drop-events'><li><a href='#'>Calendar of Events</a></li><li><a href='#'>Our Event Series</a></li></ul>";
+	 
+	 	 var pdevDropdown = "<ul class='dropdown drop-pdev'><li><a href='#'>Career Opportunities</a></li><li><a href='#'>Scholarships</a></li></ul>";
+		 
+		 	 var rescDropdown = "<ul class='dropdown drop-resc'><li><a href='#'>Recreation &amp; Wellness</a></li><li><a href='#'>Family Resources</a></li><li><a href='#'>Living in Pittsburgh</a></li><li><a href='#'>Personal Support Groups</a></li><li><a href='#'>Religious Organizations</a></li></ul>";
+	 
+	 	 var membersDropdown = "<ul class='dropdown drop-members'><li><a href='#'>Member Directory</a></li><li><a href='#'>Join</a></li><li><a href='#'>Mentorship</a></li></ul>";
 	 
 	 
-	 $("#main-nav #about").append(aboutdropdown);
-	 
+	 $("#main-nav .nav-about").append(aboutDropdown);
+	 $("#main-nav .nav-events").append(eventsDropdown);
+	 $("#main-nav .nav-pdev").append(pdevDropdown);
+	 $("#main-nav .nav-resc").append(rescDropdown);
+	 $("#main-nav .nav-members").append(membersDropdown);
+
+
+	var footer = "<div class='copyright'>&copy; 2013 Allegheny Health Network. All Rights Reserved.</div>"
+	$("footer").append(footer);
 });
