5550e2ad85d60914ca55fd9023dd367ddbc92a15
Alex Mark <ajmark@andrew.cmu.edu>
2013-12-15 19:08:05 -0500

    content

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@
 We decided to not use PHP includes to simplify the viewing process of the client
 so that they do not have to use a LAMP stack to view later
 */
+
+/* See comment at top of top-level.js for explanation of how this file differs */
+
+
 $(document).ready(function() {
 	//Creates the top nav bar
 	$("header").append("<div id='interact'></div>");
-	$("#interact").append("<h5>Interact:</h5>");
+	$("#interact").append("<h5>INTERACT:</h5>");
 	$("#interact").append("<ul id='top-nav'></ul");
 	$("#top-nav").append("<li><a href='../interact/login.html'>Member Login</a></li>");
 	$("#top-nav").append("<li><a href='../interact/join.html'>Join</a></li>");
@@ -13,12 +17,12 @@ $(document).ready(function() {
 	$("#top-nav").append("<li><a href='../interact/blog.html'>Blog</a></li>");
 	//Creates search bar
 	$("header").append("<form id='search'></form>");
-	$("#search").append("<input type='text' class='input' placeholder='Search...'>");
+	$("#search").append("<input type='text' class='input' placeholder='Search...'><input type='submit' class='searchbtn' value='Go' />");
 	//Creates the logo
 	$("header").append("<div id='logo'></div>");
 	$("#logo").append("<a href='../index.html'><img src='../img-sprites/logo.gif' alt='Allegheny Health Network Women in Medicine Group'></a>");
 	$("#logo").append("<div id='title'></div>");
-	$("#title").append("<h4>Allegheney Health Network<br><span>Women in Medicine</span></h4>");
+	$("#title").append("<h4>Allegheny Health Network<br><span>Women in Medicine</span></h4>");
 	// Creates the main nav bar
     $("header").append("<nav><ul id='main-nav'></ul></nav>");
     $("#main-nav").append("<li class='nav-home'><a href='../index.html'>Home</a></li>");
@@ -37,9 +41,8 @@ $(document).ready(function() {
 	var pdevDropdown = "<ul class='dropdown drop-pdev'><li><a href='../development/career.html'>Career Opportunities</a></li><li><a href='../development/scholarships.html'>Scholarships</a></li></ul>";
 		 
 	var rescDropdown = "<ul class='dropdown drop-resc'><li><a href='../resources/recreation.html'>Recreation &amp; Wellness</a></li><li><a href='../resources/family.html'>Family Resources</a></li><li><a href='../resources/pittsburgh.html'>Living in Pittsburgh</a></li><li><a href='../resources/support.html'>Personal Support Groups</a></li><li><a href='../resources/religious.html'>Religious Organizations</a></li></ul>";
-	 
-	var membersDropdown = "<ul class='dropdown drop-members'><li><a href='../members/directory.html'>Member Directory</a></li><li><a href='../interact/join.html'>Join</a></li><li><a href='../members/mentorship.html'>Mentorship</a></li></ul>";
-	 
+
+	var membersDropdown = "<ul class='dropdown drop-members'><li><a href='../members/directory.html'>Member Directory</a></li><li><a href='../interact/join.html'>Join WiM</a></li><li><a href='../interact/login.html'>Member Login</a></li><li><a href='../members/mentorship.html'>Mentorship</a></li></ul>";
 	 
 	 $("#main-nav .nav-about").append(aboutDropdown);
 	 $("#main-nav .nav-events").append(eventsDropdown);
@@ -49,8 +52,34 @@ $(document).ready(function() {
 
 	var footer = "<div class='copyright'>&copy; 2013 Allegheny Health Network. All Rights Reserved. <br /><a href='#'>Privacy Policy</a>  |  <a href='#'>Site Map</a>  |  <a href='../contact.html'>Contact Us</a></div>";
 	
-	var footNewsletter = "<div class='newsletter'>Join our newsletter:&nbsp;&nbsp;&nbsp;<input type='text' class='input' placeholder='Your Name' /><input type='text' class='input' placeholder='Your Email' /><input type='submit' class='submit' value='Join' /></div>";
+	var footNewsletter = "<div class='newsletter'>Join our newsletter:&nbsp;&nbsp;&nbsp;<input type='text' class='input' placeholder='Your Name' /><input type='text' class='input' placeholder='Your Email' /><input type='submit' class='submit' value='Join' /></div><a href='#' class='back-to-top'>&uarr; Back to Top</a>";
 	
 	$("footer").append(footer);
 	$("footer").append(footNewsletter);
+	
+	
+	var offset = 220;
+				var duration = 500;
+				jQuery(window).scroll(function() {
+					if (jQuery(this).scrollTop() > offset) {
+						jQuery('.back-to-top').fadeIn(duration);
+					} else {
+						jQuery('.back-to-top').fadeOut(duration);
+					}
+				});
+				
+				jQuery('.back-to-top').click(function(event) {
+					event.preventDefault();
+					jQuery('html, body').animate({scrollTop: 0}, duration);
+					return false;
+				})
 });
+
+function login() {
+    window.location.assign("loggedin.html");
+};
+
+function register() {
+    event.preventDefault();
+    window.location.assign("thankyou.html");
+};
--- js/top-level.js
+++ js/top-level.js
@@ -2,10 +2,15 @@
 We decided to not use PHP includes to simplify the viewing process of the client
 so that they do not have to use a LAMP stack to view later
 */
+
+/* This file is different from main.js because it is meant for top level pages, so all of the paths for the links are in the format    FOLDER/FILENAME.HTML
+In the main.js file, all the paths are in the format    ../FOLDER/FILENAME.HTML   because we are linking back up to the site root 
+*/
+
 $(document).ready(function() {
 	//Creates the top nav bar
 	$("header").append("<div id='interact'></div>");
-	$("#interact").append("<h5>Interact:</h5>");
+	$("#interact").append("<h5>INTERACT:</h5>");
 	$("#interact").append("<ul id='top-nav'></ul");
 	$("#top-nav").append("<li><a href='interact/login.html'>Member Login</a></li>");
 	$("#top-nav").append("<li><a href='interact/join.html'>Join</a></li>");
@@ -13,14 +18,14 @@ $(document).ready(function() {
 	$("#top-nav").append("<li><a href='interact/blog.html'>Blog</a></li>");
 	//Creates search bar
 	$("header").append("<form id='search'></form>");
-	$("#search").append("<input type='text' class='input' placeholder='Search...'>");
+	$("#search").append("<input type='text' class='input' placeholder='Search...'><input type='submit' class='searchbtn' value='Go' />");
 	//Creates the logo
 	$("header").append("<div id='logo'></div>");
 	
 		$("#logo").append("<a href='index.html'><img src='img-sprites/logo.gif' alt='Allegheny Health Network Women in Medicine Group'></a>");
 	
 	$("#logo").append("<div id='title'></div>");
-	$("#title").append("<h4>Allegheney Health Network<br><span>Women in Medicine</span></h4>");
+	$("#title").append("<h4>Allegheny Health Network<br><span>Women in Medicine</span></h4>");
 	// Creates the main nav bar
     $("header").append("<nav><ul id='main-nav'></ul></nav>");
     $("#main-nav").append("<li class='nav-home'><a href='index.html'>Home</a></li>");
@@ -40,7 +45,7 @@ $(document).ready(function() {
 		 
 	var rescDropdown = "<ul class='dropdown drop-resc'><li><a href='resources/recreation.html'>Recreation &amp; Wellness</a></li><li><a href='resources/family.html'>Family Resources</a></li><li><a href='resources/pittsburgh.html'>Living in Pittsburgh</a></li><li><a href='resources/support.html'>Personal Support Groups</a></li><li><a href='resources/religious.html'>Religious Organizations</a></li></ul>";
 	 
-	var membersDropdown = "<ul class='dropdown drop-members'><li><a href='members/directory.html'>Member Directory</a></li><li><a href='interact/join.html'>Join</a></li><li><a href='members/mentorship.html'>Mentorship</a></li></ul>";
+	var membersDropdown = "<ul class='dropdown drop-members'><li><a href='members/directory.html'>Member Directory</a></li><li><a href='interact/join.html'>Join WiM</a></li><li><a href='interact/login.html'>Member Login</a></li><li><a href='members/mentorship.html'>Mentorship</a></li></ul>";
 	 
 	 
 	 $("#main-nav .nav-about").append(aboutDropdown);
